79beacaac15f2843645bb4a4b4c428372b40f0ee
Patrick B. Gibson <patrick@fadeover.org>
2012-02-12 16:22:59 -0800

    Added some new whooffice-web API that's needed.

--- whooffice-web/whooffice.rb
+++ whooffice-web/whooffice.rb
@@ -7,6 +7,7 @@ end
   
 class Device < ActiveRecord::Base
   belongs_to :employee
+  has_many :device_in_offices
 end
   
 class DeviceInOffice < ActiveRecord::Base
@@ -19,12 +20,41 @@ class OfficeStatus < ActiveRecord::Base
   has_many :devices, :through => :device_in_offices
 end
 
+#
+# GET /in_office.json
+# Returns the names of employees currently in the office
+# 
+
 get '/in_office.json' do
-  os = OfficeStatus.find(:all, :order => "id desc", :limit => 1, :include => [:devices] )
-  puts "os devices: " + os[0].devices.inspect
-  
+  os = OfficeStatus.find(:all, :order => "id desc", :limit => 1, :include => [:devices] )  
   os.to_json(:include => :devices)
 end
+
+#
+# GET /last_seen/:employee_name.json
+# Returns the last datetime where the employee_name was in the office
+# 
+
+get '/last_seen/:employee_name.json' do
+  employee = Employee.find_by_name(params[:employee_name])
+  unless employee then return 404 end
+  
+  # We have all the devices here, so we need to search through the DeviceInOffice list to 
+  devices_in_offices = []
+  employee.devices.each do |device|
+    devices_in_offices << DeviceInOffice.find_by_device_id(device.id, :order => "id desc", :limit => 1)
+  end
+  
+  unless devices_in_offices.size > 0 then return 404 end
+  
+  sorted_devices = devices_in_offices.sort_by &:created_at
+  return {"last_seen" => sorted_devices[0].created_at}.to_json
+end
+
+#
+# POST /in_office.json
+# Takes a json dictionary with a "names" key, which has an array of device names, and stores them.
+# 
     
 post '/in_office.json' do
   request.body.rewind 
@@ -46,3 +76,20 @@ post '/in_office.json' do
     
   "OK"
 end
+
+#
+# POST /set_device_owner/:device/:employee_name.json
+# Sets :employee_name as the owner of :device. Creates any records that don't already exist.
+# 
+    
+post '/set_device_owner/:device_name/:employee_name.json' do
+  device = Device.find_or_create_by_name(params[:device_name])
+  employee = Employee.find_or_create_by_name(params[:employee_name])
+  
+  device.employee = employee
+  employee.save
+  device.save
+
+  "OK"
+end
+
