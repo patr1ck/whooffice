963afcc1d25a31207db95244eac09d7268abdb1b
Russell Osborne <russosborn@gmail.com>
2012-07-10 13:57:09 -0400

    Only display Uniq Names when returning who is in the office.

--- whooffice-web/whooffice.rb
+++ whooffice-web/whooffice.rb
@@ -55,7 +55,7 @@ get '/in_office.json' do
   os.first.devices.each do |device|
     employee_names << device.employee.name if device.employee
   end
-  {"in_office" => employee_names, "time" => os.first.created_at}.to_json
+  {"in_office" => employee_names.uniq, "time" => os.first.created_at}.to_json
 end
 
 #
