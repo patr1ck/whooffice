7e8f489a0910db5868b0aa398f80cba6cc803ac2
Russell Osborne <russosborn@gmail.com>
2012-07-10 13:57:50 -0400

    Use the right array of the json that is posted by the daemon.

--- whooffice-web/whooffice.rb
+++ whooffice-web/whooffice.rb
@@ -71,7 +71,7 @@ post '/in_office.json' do
   devices_in_office = []
   status = OfficeStatus.new
   status.save
-  devicesList["names"].each do |device_name|
+  devicesList["device_names"].each do |device_name|
     device = Device.find_or_create_by_name(device_name)
     device.save
     device_in_office = DeviceInOffice.new(:device => device, :office_status => status)
